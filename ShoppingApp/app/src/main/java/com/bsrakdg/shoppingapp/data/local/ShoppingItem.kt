74eed1df742c0db6862fbb8332b2ac57fc52d8aa
bsrAkdg <busraaakdag@gmail.com>
2020-10-08 13:59:09 +0300

    Setting up room db

---
+++ ShoppingApp/app/src/main/java/com/bsrakdg/shoppingapp/data/local/ShoppingItem.kt
@@ -0,0 +1,15 @@
+package com.bsrakdg.shoppingapp.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "shopping_items")
+data class ShoppingItem(
+    var name: String,
+    var amount: Int,
+    var price: Float,
+    var imageUrl: String,
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+)
